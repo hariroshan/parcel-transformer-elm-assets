218c24f558b0dfa68237637bbcf55fe58936ff45
hariroshan <hariroshanmail@gmail.com>
2023-12-23 16:23:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import {Transformer} from '@parcel/plugin';
+
+export default new Transformer({
+  async transform({asset}) {
+    let code = await asset.getCode();
+    let deps = code.matchAll(/ASSET_URL:(.+)\'/g);
+
+    for (let dep of deps) {
+      let depId = asset.addDependency({
+        specifier: dep[1],
+        specifierType: 'url'
+      });
+
+      code = code.replaceAll(`\'ASSET_URL:${dep[1]}\'`, `(new URL(\'${dep[1]}\', import.meta.url))`)
+    }
+    asset.setCode(code)
+    return [asset];
+  }
+});
